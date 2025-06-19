d0ea94b79febc958354553bfdabb915c4d807cf8
Tripathi <ankit.tripathi@siemens.com>
2025-06-19 18:46:14 +0530

    Updating README file

--- send_whatsapp.py
+++ send_whatsapp.py
@@ -2,15 +2,16 @@ import os
 from twilio.rest import Client
 
 def send_whatsapp_message(message):
-    account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    auth_token = os.environ['TWILIO_AUTH_TOKEN']
-    from_number = os.environ['WHATSAPP_NUMBER']
-    to_number = os.environ['TO_WHATSAPP_NUMBER']
+    # account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    # auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    # from_number = os.environ['WHATSAPP_NUMBER']
+    # to_number = os.environ['TO_WHATSAPP_NUMBER']
     
-    client = Client(account_sid, auth_token)
+    # client = Client(account_sid, auth_token)
     
-    client.messages.create(
-        body=message,
-        from_=f'whatsapp:{from_number}',
-        to=f'whatsapp:{to_number}'
-    )
+    # client.messages.create(
+    #     body=message,
+    #     from_=f'whatsapp:{from_number}',
+    #     to=f'whatsapp:{to_number}'
+    # )
+    print(f"Message to send: {message}")
